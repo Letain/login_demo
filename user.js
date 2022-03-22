b1227cad4cbc23111d9fa0f36bf0dafb80d7e44d
xianlj <xianlj@hyron.com>
2022-03-22 17:11:10 +0800

    modify

--- user.js
+++ user.js
@@ -5,8 +5,8 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'yangfang@yc1987',
-  database: 'user'
+  password: '12345678',
+  database: 'stj'
 });
 
 connection.connect();
@@ -17,7 +17,7 @@ connection.connect();
 exports.save = function (user) {
   user.user_password = md5(user.user_password)
   return new Promise((resolve,reject)=>{
-    connection.query(`INSERT INTO USERS (user_name,user_account,user_password) value ('${user.user_name}','${user.user_account}','${user.user_password}')`, function (err, res, fields) {
+    connection.query(`insert into users (user_name,user_account,user_password) value ('${user.user_name}','${user.user_account}','${user.user_password}')`, function (err, res, fields) {
       if (err) {
         // 发生错误的时候执行reject
         reject(err)
@@ -38,7 +38,7 @@ exports.findOne = function (user) {
     where.push(` and user_account = '${user.user_account}'`)
   }
   return new Promise((resolve,reject)=>{
-    connection.query(`SELECT * FROM USERS WHERE 1 = 1 ${where.join('')}`, function (err, res, fields) {
+    connection.query(`select * from users where 1 = 1 ${where.join('')}`, function (err, res, fields) {
       if (err) {
         // 发生错误的时候执行reject
         reject(err)
